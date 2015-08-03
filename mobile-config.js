0ab7286057feac76c69934c6b31d4434e975e41b
Gopi Donthireddy <gopi.don@gmail.com>
2015-08-03 22:57:26 +0530

    Init Version

--- mobile-config.js
+++ mobile-config.js
@@ -1,4 +1,19 @@
 /**
  * Created by gopi on 7/9/15.
  */
+
+App.info({
+    name: 'GSTIndiaGuru',
+    description: 'All resources relating to GST India',
+    version: '0.0.1'
+});
+
+App.icons({
+    'android_ldpi': 'resources/icons/favicon-ldpi.png',
+    'android_mdpi': 'resources/icons/favicon-mdpi.png',
+    'android_hdpi': 'resources/icons/favicon-hdpi.png',
+    'android_xhdpi': 'resources/icons/favicon-xhdpi.png'
+});
+
+
 App.accessRule("*");
